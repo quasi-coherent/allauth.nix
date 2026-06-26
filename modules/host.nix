15213303f5f32c45f6909209365d4f3cee2eb463
Daniel Donohue <d.michael.donohue@gmail.com>
2026-06-26 02:49:34 -0400

    wrong path

--- modules/host.nix
+++ modules/host.nix
@@ -1,12 +1,12 @@
 { config, ... }:
 let
-  cfg = config.allauth;
-  inherit (cfg) app targetSystem;
+  inherit (config) allauth den;
+  inherit (allauth) app targetSystem;
   inherit (app) projectName;
 in
 {
   den.aspects.${projectName}.includes = [
-    cfg.den.aspects.allauth
+    den.aspects.allauth
   ];
 
   den.hosts.${targetSystem}.${projectName} = { };
